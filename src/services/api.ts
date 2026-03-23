47ffb12738434c031cfe6c82af63cd6aef53b2b0
Umchristelle <umutonichristella17@gmail.com>
2026-03-23 12:22:14 +0200

    fixed api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
-
+console.log(import.meta.env.VITE_RAPIDAPI_KEY);
 const api = axios.create({
   baseURL: 'https://airbnb19.p.rapidapi.com',
   headers: {
     'Content-Type': 'application/json',
-    'x-rapidapi-key': 'cc7afaa5aemshf79840b454fe4e3p1c9106jsn9e32b65af7c5',
+    'x-rapidapi-key': import.meta.env.VITE_RAPIDAPI_KEY,
     'x-rapidapi-host': 'airbnb19.p.rapidapi.com',
   },
 });
